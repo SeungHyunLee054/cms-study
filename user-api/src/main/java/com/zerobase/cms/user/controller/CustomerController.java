bc39836047aae0bc580abe636bbaef72839f5314
SeungHyunLee054 <yellow13lsh@naver.com>
2023-08-11 04:54:21 +0900

    04 login customer

---
+++ user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.zerobase.cms.user.controller;
+
+import com.zerobase.cms.user.domain.customer.CustomerDto;
+import com.zerobase.cms.user.domain.model.Customer;
+import com.zerobase.cms.user.exception.CustomException;
+import com.zerobase.cms.user.exception.ErrorCode;
+import com.zerobase.cms.user.service.CustomerService;
+import com.zerobase.domain.common.UserVo;
+import com.zerobase.domain.config.JwtAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer")
+public class CustomerController {
+    private final JwtAuthenticationProvider provider;
+    private final CustomerService customerService;
+
+    @GetMapping("/getInfo")
+    public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
+        UserVo vo = provider.getUserVo(token);
+        Customer customer = customerService.findByIdAndEmail(vo.getId(), vo.getEmail())
+                .orElseThrow(() -> new CustomException(ErrorCode.NO_EXIST_USER));
+        return ResponseEntity.ok(CustomerDto.from(customer));
+    }
+}
