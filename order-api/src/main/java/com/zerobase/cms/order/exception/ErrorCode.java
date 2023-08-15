8e34a6ca0e6a574f8f77d215d87520ce9067efa1
SeungHyunLee054 <yellow13lsh@naver.com>
2023-08-16 01:53:00 +0900

    10 product modify

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    NO_EXIST_ITEM(HttpStatus.BAD_REQUEST, "아이템을 찾을 수 없습니다."),
     SAME_ITEM_NAME(HttpStatus.BAD_REQUEST,"아이템 명 중복입니다."),
     NO_EXIST_PRODUCT(HttpStatus.BAD_REQUEST, "상품을 찾을 수 없습니다.");
 
